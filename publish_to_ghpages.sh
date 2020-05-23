af1d56e978d45c0c8312f6ba44ec1997192f6e5c
Marc Abbyad <marc@medicmobile.org>
2020-05-22 21:16:16 -0400

    Resaved to make it executable from WSL

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -4,7 +4,7 @@
 
 if [ "`git status -s`" ]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "The working directory is dirty! Please commit any pending changes."
     exit 1;
 fi
 
