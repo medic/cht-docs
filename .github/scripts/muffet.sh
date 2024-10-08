f99678ae3a657ead63ed79374b1b16a391662ae4
Andra Blaj <andra@medic.org>
2024-10-08 09:54:52 +0000

    Reove deprecated flag

--- .github/scripts/muffet.sh
+++ .github/scripts/muffet.sh
@@ -17,7 +17,6 @@
 muffet http://localhost:1313 \
   --buffer-size 50000 \
   --timeout 35 \
-  --concurrency 2 \
   --ignore-fragments \
   --accepted-status-codes "200..300,403" \
   --exclude ".*demo\.app\.medicmobile\.org.*" \
