23f31b9dd8d5a4eb36fa79521bfba138f9f2527a
mrjones-plip <mrjones-plip@plip.com>
2025-02-10 11:19:42 -0800

    simplify url check scripts per feedback

--- .github/scripts/check.urls.sh
+++ .github/scripts/check.urls.sh
@@ -2,8 +2,9 @@
 
 # Function to get HTTP response code of a URL
 get_response_code() {
+    local response_code
     local url=$1
-    local response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
     echo "$response_code"
 }
 
@@ -12,36 +13,49 @@ check_meta_refresh() {
     local html_content=$1
     url=$2
     if grep -q '<meta http-equiv="refresh"' <<< "$html_content"; then
-        local redirect_url=$(grep -oP 'url=[^"]+' <<< "$html_content" | cut -d'=' -f2)
-        local redirect_response_code=$(get_response_code "$redirect_url")
+        local redirect_url
+        local redirect_response_code
+        redirect_url=$(grep -oP 'url=[^"]+' <<< "$html_content" | cut -d'=' -f2)
+        redirect_response_code=$(get_response_code "$redirect_url")
         echo "${url} Is redirected! Result is:"
         echo "    -> $redirect_url $redirect_response_code "
     fi
 }
 
 run_checks(){
-  # Loop through each URL in the file
-  while IFS= read -r url; do
-      # Get HTTP response code, if it's not 200, print it so they know
-      response_code=$(get_response_code "$url")
-      if [ "$response_code" -ne 200 ]; then
-          echo "$url $response_code"
-      fi
+    echo
+    prod_urls=$1
+    # Loop through each URL in the file
+    while IFS= read -r url; do
+        # Get HTTP response code, if it's not 200, print it so they know
+        response_code=$(get_response_code "$url")
+        if [ "$response_code" -ne 200 ]; then
+            echo "$url $response_code"
+        fi
 
-      # If response code is 200, check for meta refresh tag
-      if [ "$response_code" -eq 200 ]; then
-          html_content=$(curl -s "$url")
-          check_meta_refresh "$html_content" "$url"
-      fi
-  done < urls.txt
+        # If response code is 200, check for meta refresh tag
+        if [ "$response_code" -eq 200 ]; then
+            html_content=$(curl -s "$url")
+            check_meta_refresh "$html_content" "$url"
+        fi
+    done <<< "$prod_urls"
 }
 
-echo;echo "Are you on test branch running hugo on http://localhost:1313 and already run get.urls.sh?";echo
+get_urls_from_prod_site_map(){
+    local urls
+    # thanks https://aruljohn.com/blog/download-extract-urls-sitemaps/
+    urls=$(curl -qs https://docs.communityhealthtoolkit.org/sitemap.xml  2>&1 | grep -o "<loc>[^<]*" | sed -e 's/<[^>]*>//g')
+    urls="${urls//https:\/\/docs.communityhealthtoolkit.org/http:\/\/localhost:1313}"
+    echo "$urls"
+}
+
+echo;echo "Are you on a test branch and is hugo running on http://localhost:1313 ?";echo
 read -p "\"y\" to proceed, \"n\" to cancel " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[nN]$ ]]
 then
-  run_checks
-  echo;echo "Done!";echo
+    prod_urls=$(get_urls_from_prod_site_map)
+    url_count=$(echo "$prod_urls" | wc -l | cut -d' ' -f1)
+    run_checks "$prod_urls"
+    echo;echo "Successfully checked ${url_count} URLs!"
 fi
-
