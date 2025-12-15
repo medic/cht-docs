7d173207e1792cf334fa5da022ccbb179d397b33
mrjones <8253488+mrjones-plip@users.noreply.github.com>
2025-12-15 11:27:23 -0800

    fix 403s, bump muffet to 2.11.1 (#2097)

--- .github/scripts/muffet.sh
+++ .github/scripts/muffet.sh
@@ -5,10 +5,7 @@
 #
 # If you're doing local development and want to run link checkers locally, please use this script!
 # You'll need:
-# - install go - https://golang.org/doc/install
-# - install muffet - https://github.com/raviqqe/muffet
-# - make sure muffet is in your PATH
-# - cd ti: ./.github/scripts/
+# - cd to: ./.github/scripts/
 # - run script: ./muffet.sh
 #
 # If we have errors from this script being too aggressive (see how large downloads on
@@ -16,7 +13,7 @@
 # use extremely granular URLs so as not to exclude more than we need to.
 
 # when upgrading muffet, upload new binary here and rename so version is explicit in filename
-./2.10.9.muffet http://localhost:1313 \
+./2.11.1.muffet http://localhost:1313 \
   --buffer-size 50000 \
   --timeout 255 \
   --rate-limit 4 \
@@ -62,6 +59,11 @@
   --exclude "http[s]*://.*my.local-ip.co.*" \
   --exclude "http[s]*://.*local-ip.medicmobile.org.*" \
   --exclude "http[s]*://cht.domain.com.*" \
+  --exclude "http[s]*://www.docker.com.*" \
+  --exclude "http[s]*://blog.angular.dev/announcing-angular-v20-b5c9c06cf301" \
+  --exclude "http[s]*://askubuntu.com/a/477554" \
+  --exclude "http[s]*://medium.com.*" \
+  --exclude "http[s]*://www.pih.org.*" \
   --exclude "http[s]*://localhost:[3000|8443|5984]+" \
   --exclude "http[s]*://localhost$" \
   --exclude "http[s]*://127.*"
