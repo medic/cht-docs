e5414cc521c7269859ba8c540dae52fb3df10e58
Joshua Kuestersteffen <jkuester@kuester7.com>
2026-04-10 17:01:43 -0500

    Fix scrolling to deep links

--- assets/js/swagger.js
+++ assets/js/swagger.js
@@ -52,6 +52,23 @@ const extendDescriptionBlock = (perms, since) => (node) => {
   descWrapper.prepend(container);
 };
 
+/**
+ * Plugin to display x-permissions and x-since extensions in the operation description block.
+ *
+ * It looks for x-permissions and x-since extensions on the operation object, and if found, it adds badges to the
+ * description block of the operation. The x-permissions extension is expected to have a structure like this:
+ * {
+ *   "x-permissions": {
+ *     "hasAll": ["perm1", "perm2"],
+ *     "hasAny": ["perm3", "perm4"]
+ *   }
+ * }
+ * The x-since extension is expected to be a string with the version since which the operation is available, e.g.:
+ * {
+ *   "x-since": "v1.2.3"
+ * }
+ * @constructor
+ */
 const XExtensionsPlugin = () => ({
   wrapComponents: {
     operation: (Original, system) => (props) => {
@@ -69,13 +86,37 @@ const XExtensionsPlugin = () => ({
   }
 });
 
+/**
+ * Plugin to add an offset to the scroll position when clicking on a deep link, to account for the fixed navigation bar.
+ *
+ * Note that this has a known issue where it does not fully scroll for deep links at the very bottom of the page. This
+ * is because there is a delay in Swagger rendering the content for the selected operation. So the browser only
+ * scrolls to what it thinks is the very bottom of the page (before Swagger adds more content). To address this, we
+ * would need to somehow wait for the content to be rendered before scrolling, but there is no hook for this in Swagger.
+ * So for now, we accept this minor issue.
+ * @constructor
+ */
+const ScrollOffsetPlugin = () => ({
+  statePlugins: {
+    layout: {
+      wrapActions: {
+        scrollToElement: () => ref => {
+          const offset = document.querySelector('nav')?.offsetHeight || 0;
+          const top = ref.getBoundingClientRect().top + window.scrollY - offset;
+          window.scrollTo({ top, behavior: 'smooth' });
+        }
+      }
+    }
+  }
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   SwaggerUIBundle({
     dom_id: '#swagger-ui',
     // TODO Update
     url: 'https://gist.githubusercontent.com/jkuester/a738de6aa6f96e5957b1f4ce56a3692a/raw/openapi.json',
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-    plugins: [XExtensionsPlugin],
+    plugins: [XExtensionsPlugin, ScrollOffsetPlugin],
     filter: true,
     operationsSorter: 'alpha',
     tagsSorter: 'alpha',
